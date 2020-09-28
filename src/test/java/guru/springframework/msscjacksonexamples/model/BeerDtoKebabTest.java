3104e69ac245e019652484a5a4744a64d899a2d5
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2020-09-28 12:54:25 -0500

    Updated BeerDto's ID to use custom JSON property name

--- src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoKebabTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoKebabTest.java
@@ -24,7 +24,7 @@ public class BeerDtoKebabTest extends BaseTest {
 
     @Test
     void testKebabDeserialize() throws Exception {
-        String json = "{\"id\":\"5ad72ea4-be3b-4ae2-bb30-75b070f1832e\",\"beer-name\":\"BeerName\",\"beer-style\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"created-date\":\"2020-09-25T17:02:59.0287558-05:00\",\"last-updated-date\":\"2020-09-25T17:02:59.0287558-05:00\"}";
+        String json = "{\"beerId\":\"5ad72ea4-be3b-4ae2-bb30-75b070f1832e\",\"beer-name\":\"BeerName\",\"beer-style\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"created-date\":\"2020-09-25T17:02:59.0287558-05:00\",\"last-updated-date\":\"2020-09-25T17:02:59.0287558-05:00\"}";
 
         BeerDto dto = objectMapper.readValue(json, BeerDto.class);
 
